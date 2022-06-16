efcd06b6a2b9bcbcaf67921ad4aa0a1a7611e778
mszubillag24ucema <mszubillag24@ucema.edu.ar>
2022-06-16 16:32:40 -0300

    -m "holis"

--- carteras_obj.py
+++ carteras_obj.py
@@ -1,46 +1,46 @@
 class Carteras:
-    def __init__(self, id, date_created, marca_cartera, color):
+    def __init__(self, id, fecha_ingreso, marca_cartera, color):
         self.id = id
-        self.date_created = date_created
+        self.fecha_ingreso = fecha_ingreso
         self.marca_cartera = marca_cartera
         self.color = color
 
 
 class Mochila(carteras):
-    def __init__(self, id, date_created, modelo_cartera, marca_cartera, color):
-        super().__init__(id, date_created, modelo_cartera, marca_cartera, color)
+    def __init__(self, id, fecha_ingreso, modelo_cartera, marca_cartera, color):
+        super().__init__(id, fecha_ingreso, modelo_cartera, marca_cartera, color)
         self.id = id
-        self.date_created = date_created
+        self.fecha_ingreso = fecha_ingreso
         self.modelo_cartera = modelo_cartera
         self.marca_cartera = marca_cartera
         self.color = color
 
 
 class CrossBody(carteras):
-    def __init__(self, id, date_created, modelo_cartera, marca_cartera, color):
-        super().__init__(id, date_created, modelo_cartera, marca_cartera, color)
+    def __init__(self, id, fecha_ingreso, modelo_cartera, marca_cartera, color):
+        super().__init__(id, fecha_ingreso, modelo_cartera, marca_cartera, color)
         self.id = id
-        self.date_created = date_created
+        self.fecha_ingreso = fecha_ingreso
         self.modelo_cartera = modelo_cartera
         self.marca_cartera = marca_cartera
         self.color = color
 
 
 class Clutch(carteras):
-    def __init__(self, id, date_created, modelo_cartera, marca_cartera, color):
-        super().__init__(id, date_created, modelo_cartera, marca_cartera, color)
+    def __init__(self, id, fecha_ingreso, modelo_cartera, marca_cartera, color):
+        super().__init__(id, fecha_ingreso, modelo_cartera, marca_cartera, color)
         self.id = id
-        self.date_created = date_created
+        self.fecha_ingreso = fecha_ingreso
         self.modelo_cartera = modelo_cartera
         self.marca_cartera = marca_cartera
         self.color = color
 
 
 class Totebag(carteras):
-    def __init__(self, id, date_created, modelo_cartera, marca_cartera, color):
-        super().__init__(id, date_created, modelo_cartera, marca_cartera, color)
+    def __init__(self, id, fecha_ingreso, modelo_cartera, marca_cartera, color):
+        super().__init__(id, fecha_ingreso, modelo_cartera, marca_cartera, color)
         self.id = id
-        self.date_created = date_created
+        self.fecha_ingreso = fecha_ingreso
         self.modelo_cartera = modelo_cartera
         self.marca_cartera = marca_cartera
         self.color = color
