c932244e40cc0a166a70d31c5415bf432efdd016
mcmarkstein <melinamarkstein@gmail.com>
2022-06-16 15:42:51 -0300

    nuevo commit

---
+++ clientes.py
@@ -0,0 +1,47 @@
+clientes = [
+  {
+    "id_cliente": "12345",
+    "Nombre": "Mario",
+    "Markstein": "Arjona",
+    "DNI": "23700981",
+    "Telefono": "01148723458",
+    "Email": "marjona@gmail.com",
+    "Estado": "ACTIVO",
+  },
+{
+    "id_cliente": "98765",
+	"Nombre": "Alejandra",
+	"Markstein": "Sanchez",
+	"DNI": "38407221",
+	"Telefono": "01184926574",
+	"Email": "asanchez@gmail.com",
+	"Estado": "ACTIVO",
+  },
+{
+    "id_cliente": "12345",
+	"Nombre": "Manuel",
+	"Markstein": "Belgrano",
+	"DNI": "124589638",
+	"Telefono": "01132679472",
+	"Email": "mbelgrano@gmail.com",
+	"Estado": "INACTIVO",
+  },
+{
+    "id_cliente": "00751",
+	"Nombre": "Julieta",
+	"Markstein": "Mandalorian",
+	"DNI": "37889675",
+	"Telefono": "01154399633",
+	"Email": "mandalirian1@gmail.com",
+	"Estado": "ACTIVO",
+  },
+  {
+    "id_cliente": "00843",
+	"Nombre": "Soledad",
+	"Markstein": "Gorgelin",
+	"DNI": "33425786",
+	"Telefono": "01166734324",
+	"Email": "gorge_sole@gmail.com",
+	"Estado": "INACTIVO",
+  }
+]
--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-# This is a sample Python script.
+import json
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import make_response
+
+from clientes import Cliente, generador_id_cliente
+from carteras import Cartera, Totebag, CrossBody, Clutch, Mochila
+
+app = Flask(__name__)
 
 
+@app.route("/api/el_galpo_de_lujo/generar_usuario/, methods = ['POST']")
+def crear_cliente():
+    cliente = request.json
+
+    try:
+        nuevo_cliente = Cliente(
+            generador_id_cliente(),
+            cliente['Nombre'],
+            cliente['Apellido'],
+            cliente['DNI'],
+            cliente['Telefono'],
+            cliente['email'],
+            cliente['Estado']
+        )
+
+    cliente.append(new_client)
+
+    except KeyError as key_err:
+    missing_param = (key_err.__str__())
+    return jsonify(
+        error_code=400,
+        error_description="Bad request",
+        error_body=missing_param
+    ), 400
+
+    return jsonify(nuevo_cliente.serialize())
