79c1f06b569178a3578a310b00a010b995b9cec6
mcmarkstein <melinamarkstein@gmail.com>
2022-06-18 10:47:36 -0300

    nuevo commit

--- bd_carteras.py
+++ bd_carteras.py
@@ -1,142 +1,181 @@
 [
     {
-    "id": "0001",
-    "fecha_ingreso": "21/07/2020",
-    "modelo": "Clutch",
-    "marca": "Gucci",
-    "color": "roja"
+        "id": "0001",
+        "fecha_ingreso": "21/07/2020",
+        "modelo": "Clutch",
+        "marca": "Gucci",
+        "color": "roja",
+        "tipo_de_cambio": "EUR",
+        "precio": 2000
     },
     {
-    "id": "0002",
-    "fecha_ingreso": "22/06/2019",
-    "modelo": "Totebag",
-    "marca": "Louis Vuitton",
-    "color": "marrón"
+        "id": "0002",
+        "fecha_ingreso": "22/06/2019",
+        "modelo": "Totebag",
+        "marca": "Louis Vuitton",
+        "color": "marrón",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0003",
-    "fecha_ingreso": "9/12/2018",
-    "modelo": "Clutch",
-    "marca": "Chanel",
-    "color": "negra"
+        "id": "0003",
+        "fecha_ingreso": "9/12/2018",
+        "modelo": "Clutch",
+        "marca": "Chanel",
+        "color": "negra",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0004",
-    "fecha_ingreso": "4/08/2020",
-    "modelo": "Cross Body",
-    "marca": "Prada",
-    "color": "negra"
+        "id": "0004",
+        "fecha_ingreso": "4/08/2020",
+        "modelo": "Cross Body",
+        "marca": "Prada",
+        "color": "negra",
+         "tipo_de_cambio": "",
+         "precio":
     },
     {
-    "id": "0005",
-    "fecha_ingreso": "13/05/2020",
-    "modelo": "Mochila",
-    "marca": "Fendi",
-    "color": "azul"
-},
-{
-    "id": "0006",
-    "fecha_ingreso": "18/03/2021",
-    "modelo": "Totebag",
-    "marca": "Hermés",
-    "color": "marrón"
+        "id": "0005",
+        "fecha_ingreso": "13/05/2020",
+        "modelo": "Mochila",
+        "marca": "Fendi",
+        "color": "azul",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0007",
-    "fecha_ingreso": "17/06/2022",
-    "modelo": "Clutch",
-    "marca": "Louis Vuitton",
-    "color": "naranja"
+        "id": "0006",
+        "fecha_ingreso": "18/03/2021",
+        "modelo": "Totebag",
+        "marca": "Hermés",
+        "color": "marrón",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0008",
-    "fecha_ingreso": "13/05/2021",
-    "modelo": "Mochila",
-    "marca": "Gucci",
-    "color": "verde"
-},
-{
-    "id": "0009",
-    "fecha_ingreso": "7/09/2021",
-    "modelo": "Cross Body",
-    "marca": "Prada",
-    "color": "rosa"
+        "id": "0007",
+        "fecha_ingreso": "17/06/2022",
+        "modelo": "Clutch",
+        "marca": "Louis Vuitton",
+        "color": "naranja",
+        "tipo_de_cambioa": "",
+        "precio":
     },
     {
-    "id": "0010",
-    "fecha_ingreso": "3/08/2021",
-    "modelo": "Totebag",
-    "marca": "Chanel",
-    "color": "rosa"
+        "id": "0008",
+        "fecha_ingreso": "13/05/2021",
+        "modelo": "Mochila",
+        "marca": "Gucci",
+        "color": "verde",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0011",
-    "fecha_ingreso": "1/10/2021",
-    "modelo": "Cross Body",
-    "marca": "Louis Vuitton",
-    "color": "amarillo"
+        "id": "0009",
+        "fecha_ingreso": "7/09/2021",
+        "modelo": "Cross Body",
+        "marca": "Prada",
+        "color": "rosa",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0012",
-    "fecha_ingreso": "8/12/2021",
-    "modelo": "Clutch",
-    "marca": "Hermés",
-    "color": "beige"
+        "id": "0010",
+        "fecha_ingreso": "3/08/2021",
+        "modelo": "Totebag",
+        "marca": "Chanel",
+        "color": "rosa",
+        "tipo_de_cambio": "",
+        "precio":
+    {
+        "id": "0011",
+        "fecha_ingreso": "1/10/2021",
+        "modelo": "Cross Body",
+        "marca": "Louis Vuitton",
+        "color": "amarillo",
+        "tipo_de_cambiolo": "",
+        "precio":
     },
     {
-    "id": "0013",
-    "fecha_ingreso": "15/09/2021",
-    "modelo": "Mochila",
-    "marca": "Prada",
-    "color": "gris"
+        "id": "0012",
+        "fecha_ingreso": "8/12/2021",
+        "modelo": "Clutch",
+        "marca": "Hermés",
+        "color": "beige",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0014",
-    "fecha_ingreso": "7/11/2021",
-    "modelo": "Cross Body",
-    "marca": "Prada",
-    "color": "plateada"
+        "id": "0013",
+        "fecha_ingreso": "15/09/2021",
+        "modelo": "Mochila",
+        "marca": "Prada",
+        "color": "gris",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0015",
-    "fecha_ingreso": "17/04/2022",
-    "modelo": "Clutch",
-    "marca": "Chanel",
-    "color": "marrón"
+        "id": "0014",
+        "fecha_ingreso": "7/11/2021",
+        "modelo": "Cross Body",
+        "marca": "Prada",
+        "color": "plateado",
+        "tipo_de_cambioda": "",
+        "precio":
     },
     {
-    "id": "0016",
-    "fecha_ingreso": "4/04/2022",
-    "modelo": "Totebag",
-    "marca": "Hermés",
-    "color": "negro"
+        "id": "0015",
+        "fecha_ingreso": "17/04/2022",
+        "modelo": "Clutch",
+        "marca": "Chanel",
+        "color": "marrón",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0017",
-    "fecha_ingreso": "8/12/2021",
-    "modelo": "Clutch",
-    "marca": "Hermés",
-    "color": "beige"
+        "id": "0016",
+        "fecha_ingreso": "4/04/2022",
+        "modelo": "Totebag",
+        "marca": "Hermés",
+        "color": "negro",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0018",
-    "fecha_ingreso": "9/05/2021",
-    "modelo": "Cross body",
-    "marca": "Gucci",
-    "color": "beige"
+        "id": "0017",
+        "fecha_ingreso": "8/12/2021",
+        "modelo": "Clutch",
+        "marca": "Hermés",
+        "color": "beige",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0019",
-    "fecha_ingreso": "12/08/2021",
-    "modelo": "Mochila",
-    "marca": "Chanel",
-    "color": "blanco"
+        "id": "0018",
+        "fecha_ingreso": "9/05/2021",
+        "modelo": "Cross body",
+        "marca": "Gucci",
+        "color": "beige",
+        "tipo_de_cambio": "",
+        "precio":
     },
     {
-    "id": "0020",
-    "fecha_ingreso": "9/03/2022",
-    "modelo": "Totebag",
-    "marca": "Fendi",
-    "color": "amarillo"
+        "id": "0019",
+        "fecha_ingreso": "12/08/2021",
+        "modelo": "Mochila",
+        "marca": "Chanel",
+        "color": "blanco",
+        "tipo_de_cambio": "",
+        "precio":
+    },
+    {
+        "id": "0020",
+        "fecha_ingreso": "9/03/2022",
+        "modelo": "Totebag",
+        "marca": "Fendi",
+        "color": "amarillo",
+        "tipo_de_cambio": "",
+        "precio":
     }
 ]
--- load_cartera.py
+++ load_cartera.py
@@ -7,6 +7,7 @@ def cargar_carteras():
     with open('/bd_carteras.py', 'r') as file:
         bd_carteras = json.load(file)
         for cartera in bd_carteras:
+
             carteras.append(
                 Carteras(
                     cartera['id'],
