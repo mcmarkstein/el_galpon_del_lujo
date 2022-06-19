bb43b5986bd3f6e8e66db8134c280ae7908f3afe
mcmarkstein <melinamarkstein@gmail.com>
2022-06-19 19:12:29 -0300

    nuevo commit

--- crear_base_clientes.py
+++ crear_base_clientes.py
@@ -1,5 +1,13 @@
 import sqlite3
 
+clientes = [
+    ('12345', 'Mario', 'Arjona', '23700981', '01148723458', 'marjona@gmail.com', 'ACTIVO'),
+    ('98765','Alejandra','Sanchez','38407221','01184926574','asanchez@gmail.com','ACTIVO'),
+    ('12345', 'Manuel', 'Belgrano', '124589638', '01132679472', 'mbelgrano@gmail.com','INACTIVO'),
+    ('00751', 'Julieta', 'Mandalorian', '37889675', '01154399633','mandalirian1@gmail.com','ACTIVO'),
+    ('00843', 'Soledad', 'Gorgelin', '33425786', '01166734324', 'gorge_sole@gmail.com','INACTIVO')
+]
+
 conexion = sqlite3.connect('./clientesBBDD.db')
 
 cursor = conexion.cursor()
@@ -23,12 +31,8 @@ cursor.execute(sentenciaSQL)
 conexion.commit()
 '''
 '''
-sentenciaSQL2 = "INSERT INTO clientes_2 VALUES ('12345', 'Mario', 'Arjona', '23700981', '01148723458', 'marjona@gmail.com', 'ACTIVO');"
-#sentenciaSQL2 = "INSERT INTO clientes_2 VALUES ('98765','Alejandra','Sanchez','38407221','01184926574','asanchez@gmail.com','ACTIVO');"
-#sentenciaSQL2 = "INSERT INTO clientes_2 VALUES ('12345', 'Manuel', 'Belgrano', '124589638', '01132679472', 'mbelgrano@gmail.com','INACTIVO');"
-#sentenciaSQL2 = "INSERT INTO clientes_2 VALUES ('00751', 'Julieta', 'Mandalorian', '37889675', '01154399633','mandalirian1@gmail.com','ACTIVO');"
-#sentenciaSQL2 = "INSERT INTO clientes_2 VALUES ('00843', 'Soledad', 'Gorgelin', '33425786', '01166734324', 'gorge_sole@gmail.com','INACTIVO');"
-cursor.execute(sentenciaSQL2)
+sentenciaSQL2 = "INSERT INTO clientes_2 VALUES (?,?,?,?,?,?,?)
+cursor.executemany(sentenciaSQL2, clientes)
 
 conexion.commit()
 
