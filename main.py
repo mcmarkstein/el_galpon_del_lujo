aa21a16d9c214fdc7bad2e03916da7ea4a76ec98
verobura <veronica.valentinab@gmail.com>
2022-06-22 18:14:11 -0300

    solucionado

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def ver_cartera_por_marca(marca):
     for cartera in carteras:
         if cartera.marca == marca:
             carteras_marca.append(cartera)
-    return jsonify([cartera.serialize() for cartera in carteras_marca])
-
+            return jsonify([cartera.serialize() for cartera in carteras_marca])
+    return "No se encontro la marca ingresada."
 
 
 @app.route("/api/el_galpon_de_lujo/carteras/<id>", methods=['GET'])     #ver una cartera por id
@@ -63,7 +63,7 @@ def ver_cartera_por_id(id):
     for cartera in carteras:
         if cartera.id == id:
             return jsonify(cartera.serialize())
-
+    return jsonify({f'El producto con el id {id}': 'No existe'})
 
 
 @app.route("/api/el_galpon_de_lujo/precio_ARS/<id>", methods=['GET'])       #Ver el precio de una cartera en ARS
@@ -81,8 +81,7 @@ def precio_ARS(id):
             rsps_json = response.json()
 
             return {f'El precio original en {cartera.tipo_de_cambio} es': cartera.precio, 'El precio en ARS es' : rsps_json["result"] }
-
-
+    return jsonify({f'El producto con el id {id}': 'No existe'})
 
 @app.route("/api/el_galpon_de_lujo/carrito/<id_cliente>/agregar/<id>", methods=['PUT'])       #Agregar a carrito por id
 def agregar_a_carrito(id_cliente, id):
@@ -94,7 +93,6 @@ def agregar_a_carrito(id_cliente, id):
     return jsonify({f'El id de la cartera: {id}  y/o el id del cliente: {id_cliente}': 'Han sido escritos incorrectamente o no existen'})
 
 
-
 @app.route("/api/el_galpon_de_lujo/carrito/<id_cliente>/eliminar/<id>", methods=['DELETE'])      #Eliminar producto de carrito por id
 def eliminar_de_carrito(id_cliente, id):
     for cliente in clientes:
@@ -103,9 +101,7 @@ def eliminar_de_carrito(id_cliente, id):
                 if producto.id == id:
                     cliente.carrito.remove(producto)
                     return jsonify({'Busqueda': id, 'Estado': 'Se ha eliminado el producto'})
-                return jsonify({f'El producto con el id {id}': 'No esta en el carrito o no exite'})
-
-
+            return jsonify({f'El producto con el id {id}': 'No esta en el carrito o no existe'})
 
 
 @app.route("/api/el_galpon_de_lujo/carrito/<id_cliente>", methods = ['GET'])        #ver carrito
@@ -113,8 +109,7 @@ def ver_carrito(id_cliente):
     for cliente in clientes:
         if cliente.id_cliente == id_cliente:
             return jsonify([producto.serialize() for producto in cliente.carrito])
-        else:
-            return jsonify({'Error': f'Cliente {id_cliente} no encontrado'})
+    return jsonify({f'Error.Cliente {id_cliente}': 'No encontrado'})
 
 
 @app.route("/api/el_galpon_de_lujo/terminar_compra/<id_cliente>", methods=['GET'])         #terminar compra
